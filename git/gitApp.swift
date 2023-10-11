91ea80655ade7756e5d8b078ffc02375cc7d2733
Manohar <manoharmanu549@gmail.com>
2023-10-11 10:00:03 -0500

    Initial Commit

---
+++ git/gitApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitApp.swift
+//  git
+//
+//  Created by Manohar on 10/11/23.
+//
+
+import SwiftUI
+
+@main
+struct gitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
